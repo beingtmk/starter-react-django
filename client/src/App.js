0ed913749fac26f6e5c35161f4a25e64e9b423fb
Mithilesh Kohale <beingtmk@gmail.com>
2019-01-13 12:09:16 +0530

    added basic redux and homepage+posts

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+
+import { Route, Switch } from "react-router-dom";
+import Home from "./components/scenes/Home";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+        {/* <Header
+          currentUser={this.props.currentUser}
+          userType={this.props.userType}
+        /> */}
+        <Switch>
+          <Route exact path="/" component={Home} />
+          {/* <Route path="/login" render={props => <Login {...props} />} />
+          <Route path="/categories" component={Categories} /> */}
+        </Switch>
+      </React.Fragment>
     );
   }
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import store from "./store";
+import history from "./history";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from "./App";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter history={history}>
+      <App />
+    </BrowserRouter>{" "}
+  </Provider>,
+  document.getElementById("root")
+);
